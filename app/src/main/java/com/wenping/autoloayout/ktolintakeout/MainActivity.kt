07deaf7c014d9600f1163b61d5ffa2b99f28e94a
wenping <wenpingkk@163.com>
2018-04-28 11:23:58 +0800

    002 资源文件以及 底部导航栏功能

--- app/src/main/java/com/wenping/autoloayout/ktolintakeout/MainActivity.kt
+++ app/src/main/java/com/wenping/autoloayout/ktolintakeout/MainActivity.kt
@@ -1,12 +1,53 @@
-package com.wenping.autoloayout.ktolintakeout
+package com.wenping.ktolintakeout
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import com.wenping.autoloayout.ktolintakeout.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initBottomBar()
+
+    }
+
+    private fun initBottomBar() {
+
+        for (i in 0 until main_bottom_bar.childCount) {
+            main_bottom_bar.getChildAt(i).setOnClickListener {
+                changeIndex(i)
+            }
+        }
+
+    }
+
+    private fun changeIndex(index: Int) {
+        for (i in 0 until main_bottom_bar.childCount) {
+            val child = main_bottom_bar.getChildAt(i)
+            if (i == index) {
+                //选中,则禁用效果
+                setEnable(child,false)
+            } else {
+                //没选中,则不禁用
+                setEnable(child,true)
+            }
+        }
+
+
+    }
+
+    private fun setEnable(child: View, isEnable: Boolean) {
+        child.isEnabled = isEnable
+        if (child is ViewGroup) {
+            for (i in 0 until child.childCount) {
+                child.getChildAt(i).isEnabled = isEnable
+            }
+        }
     }
 }
